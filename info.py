ee92a8f358e08cff145c995b9401f565ef44188a
Bow ! Bow ! <105433790+mafiatamilan@users.noreply.github.com>
2023-07-02 11:01:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ PICS = (environ.get('PICS' ,'https://te.legra.ph/file/068c7b2fbcfddbc1db13a.jpg 
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5179005671 ').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
